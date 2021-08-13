e3fc4d9c66d3fd4f4e36fcad95ab7ba23c557d56
KarenDanielyan <karendanielyan66@gmail.com>
2021-08-14 02:01:09 +0400

    Start of implementation. Added the declarations of the functions.

---
+++ A2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if( args.Length > )
+            string expr = getExpression();
+            
+        }
+
+        static string getExpression()
+        {
+            return Console.ReadLine();
+        }
+
+
+
+
+        static List<String> arg = new List<String>({"-v", "n"});
+        static oprator[] myOperators = new oprator[7];
+
+        static float Sum()
+        {
+            return 0;
+        }
+        static float Multiply()
+        {
+            return 0;
+        }
+        static float Divide()
+        {
+            return 0;
+        }
+        static float Substract()
+        {
+            return 0;
+        }
+        static float Mod()
+        {
+            return 0;
+        }
+        static float Pow()
+        {
+            return 0;
+        }
+        static float Fac()
+        {
+            return 0;
+        }
+
+        struct oprator{
+             public char value;
+             public int peredence;
+        }
+
+    }
+}
