f9da372c1be16197298d4b7dc9747a5f54a53fea
KarenDanielyan <karendanielyan66@gmail.com>
2021-08-16 02:45:13 +0400

    Added UnaryExpressionSyntax implementation.

---
+++ A2/CodeAnalysis/UnaryExpressionSyntax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+namespace A2.CodeAnalysis
+{
+    sealed class UnaryExpressionSyntax : ExpressionSyntax
+    {
+        public UnaryExpressionSyntax(ExpressionSyntax operand, SyntaxToken operatorToken)
+        {
+            Operand = operand;
+            OperatorToken = operatorToken;
+        }
+
+        public override SyntaxType Type => SyntaxType.UnaryExpression;
+        public ExpressionSyntax Operand { get; }
+        public SyntaxToken OperatorToken { get; }
+
+        public override IEnumerable<SyntaxNode> GetChildern()
+        {
+            yield return OperatorToken;
+            yield return Operand;
+        }
+    }
+}
