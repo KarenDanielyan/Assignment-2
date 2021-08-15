59ada914165dc3fae54f33c2e801f671b3a910bb
KarenDanielyan <karendanielyan66@gmail.com>
2021-08-15 19:19:32 +0400

    Implemented Parser. Can now output Chomsky's tree.

--- A2/Program.cs
+++ A2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace A2
 {
@@ -21,7 +22,22 @@ namespace A2
             {
                 Console.Write("> ");
                 var line = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(line))
+                    return;
 
+                var parser = new Parser(line);
+                var expression = parser.Parse();
+
+
+                //Stying
+                var color = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+                    TreePrint(expression);
+
+                Console.ForegroundColor = color;
+
+                /*
                 var tokens = new Tokenizer(line);
                 while(true)
                 {
@@ -38,11 +54,41 @@ namespace A2
                 }
 
                 Console.WriteLine();
+                */
             }
 
         }
 
 //      BASIC FUNCTIONS
+
+        static void TreePrint(SyntaxNode node, string indent = "", bool isLast = true)
+        {
+            //├──
+            //│
+            //└──
+            var marker = isLast ? "└──" : "├──";
+
+            Console.Write(indent);
+            Console.Write(marker);
+            Console.Write(node.Type);
+            if(node is SyntaxToken t && t.Value != null)
+            {
+                Console.Write(" ");
+                Console.Write(t.Value);
+            }
+
+            Console.WriteLine();
+
+            indent += isLast ? "     " : "│    ";
+
+            var lastChild = node.GetChildern().LastOrDefault();
+
+            foreach (var c_node in node.GetChildern())
+            {
+                TreePrint(c_node, indent, c_node == lastChild);
+            }    
+        }
+
         static void CheckArguments(string[] args)
         {
             if (args.Length > arg.Count)
@@ -61,7 +107,8 @@ namespace A2
 
     }
 
-    public enum SyntaxType
+//  Initialization types
+    enum SyntaxType
     {
         NumberToken,
         SpaceToken,
@@ -72,10 +119,20 @@ namespace A2
         OpenParenToken,
         CloseParenToken,
         BadToken,
-        EndofLineToken
+        EndofLineToken,
+        NumberExpression,
+        BinaryExpression
     }
 
-    public class SyntaxToken
+    abstract class SyntaxNode
+    {
+        public abstract SyntaxType Type { get; }
+
+        public abstract IEnumerable<SyntaxNode> GetChildern();
+
+    }
+
+    class SyntaxToken : SyntaxNode
     {
         public SyntaxToken(SyntaxType type, int position, string text, object value)
         {
@@ -85,12 +142,65 @@ namespace A2
             Value = value;
         }
 
-        public SyntaxType Type { get; }
+        public override SyntaxType Type { get; }
         public int Position { get; }
         public string Text { get; }
         public object Value { get; }
+
+        public override IEnumerable<SyntaxNode> GetChildern()
+        {
+            return Enumerable.Empty<SyntaxNode>();
+        }
     }
 
+    abstract class ExpressionSyntax : SyntaxNode
+    {
+        public ExpressionSyntax()
+        {
+                
+        }
+    }
+
+    sealed class NumberExpressionSyntax : ExpressionSyntax
+    {
+        public NumberExpressionSyntax(SyntaxToken numberToken)
+        {
+            NumberToken = numberToken;
+        }
+
+        public override SyntaxType Type => SyntaxType.NumberExpression;
+
+        public SyntaxToken NumberToken { get; }
+
+        public override IEnumerable<SyntaxNode> GetChildern()
+        {
+            yield return NumberToken; 
+        }
+    }
+
+    sealed class BinaryExpressionSyntax : ExpressionSyntax
+    {
+        public BinaryExpressionSyntax(ExpressionSyntax leftToken, ExpressionSyntax rightToken, SyntaxToken operatorToken)
+        {
+            LeftToken = leftToken;
+            RightToken = rightToken;
+            OperatorToken = operatorToken;
+        }
+
+        public override SyntaxType Type => SyntaxType.BinaryExpression;
+        public ExpressionSyntax LeftToken { get; }
+        public ExpressionSyntax RightToken { get; }
+        public SyntaxToken OperatorToken { get; }
+
+        public override IEnumerable<SyntaxNode> GetChildern()
+        {
+            yield return LeftToken;
+            yield return OperatorToken;
+            yield return RightToken;
+        }
+    }
+
+//  Lexer
     class Tokenizer
     {
         private readonly string _text;
@@ -183,8 +293,81 @@ namespace A2
         }
     }
 
+    //  Parser
     class Parser
     {
+        private readonly SyntaxToken[] _tokens;
+        private int _position;
+        public Parser(string text)
+        {
+            var tokens = new List<SyntaxToken>();
 
+            var tokenizer = new Tokenizer(text);
+            SyntaxToken token;
+
+            do
+            {
+                token = tokenizer.NextToken();
+
+                if (token.Type != SyntaxType.SpaceToken && token.Type != SyntaxType.BadToken)
+                    tokens.Add(token);
+            }
+            while (token.Type != SyntaxType.EndofLineToken);
+
+
+            _tokens = tokens.ToArray();
+
+        }
+        private SyntaxToken Peek(int offset)
+        {
+            var index = _position + offset;
+
+            if (index >= _tokens.Length)
+            {
+                return _tokens[_tokens.Length - 1];
+            }
+            else
+                return _tokens[index];
+        }
+
+        private SyntaxToken Current => Peek(0);
+
+        private SyntaxToken NextToken()
+        {
+            var current = Current;
+            _position++;
+
+            return current;
+        }
+
+        private SyntaxToken Match(SyntaxType type)
+        {
+            if (Current.Type == type)
+                return NextToken();
+            else
+                return new SyntaxToken(type, Current.Position, null, null);
+        }
+
+        public ExpressionSyntax Parse()
+        {
+            var l_prime = ParsePrimeExpression();
+
+            while( Current.Type == SyntaxType.AdditionToken ||
+                    Current.Type == SyntaxType.SubstractToken)
+            {
+                var operatorToken = NextToken();
+                var r_prime = ParsePrimeExpression();
+
+                l_prime = new BinaryExpressionSyntax(l_prime, r_prime, operatorToken);
+            }
+
+            return l_prime;
+        }
+
+        private ExpressionSyntax ParsePrimeExpression()
+        {
+            var numberToken = Match(SyntaxType.NumberToken);
+            return new NumberExpressionSyntax(numberToken);
+        }
     }
 }
