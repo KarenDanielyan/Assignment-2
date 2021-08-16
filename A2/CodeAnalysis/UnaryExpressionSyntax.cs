4d685b22e29f41712538081839faddd263559c9c
KarenDanielyan <karendanielyan66@gmail.com>
2021-08-16 15:17:59 +0400

    Implemented UnaryExpressionSyntax type.

---
+++ A2/CodeAnalysis/UnaryExpressionSyntax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+namespace A2.CodeAnalysis
+{
+    sealed class UnaryExpressionSyntax : ExpressionSyntax
+    {
+        public UnaryExpressionSyntax(ExpressionSyntax operand, SyntaxToken operatorToken)
+        {
+            Operand = operand;
+            OperatorToken = operatorToken;
+        }
+
+        public override SyntaxType Type => SyntaxType.BinaryExpression;
+        public ExpressionSyntax Operand { get; }
+        public SyntaxToken OperatorToken { get; }
+
+        public override IEnumerable<SyntaxNode> GetChildern()
+        {
+            yield return Operand;
+            yield return OperatorToken;
+        }
+    }
+}
