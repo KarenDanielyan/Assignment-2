659520fdb1e5bf568f39434cca9b1826ca7edd5d
KarenDanielyan <karendanielyan66@gmail.com>
2021-08-16 15:18:32 +0400

    Corrected the operator precedences.

--- A2/CodeAnalysis/SyntaxPriority.cs
+++ A2/CodeAnalysis/SyntaxPriority.cs
@@ -2,6 +2,18 @@
 {
     internal static class SyntaxPriority
     {
+        public static int GetUnaryOperatorPrecedence(this SyntaxType type)
+        {
+            switch (type)
+            {
+                case SyntaxType.AdditionToken:
+                case SyntaxType.SubstractToken:
+                    return 3;
+                default:
+                    return 0;
+            }
+        }
+
         public static int GetBinaryOperatorPrecedence(this SyntaxType type)
         {
             switch (type)
